b5eb6c1d8b875fe31fd7530fba7a4feffc7d9b8e
manh0403 <85344744+manh0403@users.noreply.github.com>
2021-06-04 18:32:21 +0700

    Update config.js

--- config.js
+++ config.js
@@ -11,9 +11,9 @@ const CONFIG = {
     btnNo: 'Không, Anh trai à :3',
     question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
     btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay call avernight nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
+    reply: 'Yêu thì yêu mà không yêu thì yêu <3333333333333333333333333',
+    mess: 'Anh biết mà 😚😚😚. Yêu em nhiều nhiều 😘😘😘',
+    messDesc: 'Tối nay call overnight nhé công chúa.',
+    btnAccept: 'Đồng ý lun <3',
     messLink: 'http://fb.com/manh.vutien.31105'
 }
